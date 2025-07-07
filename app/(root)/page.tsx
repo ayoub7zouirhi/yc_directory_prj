5e4ae6fab20e8d21904982b5347958fd81aaf8dd
Ayoub <azouirhi2@gmail.com>
2025-07-07 18:35:51 +0100

    'sooskosd'

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Readme from "../component/readme";
+
+export default function Home() {
+  return (
+    <>
+      <h1>ofe</h1>
+      <Readme />
+      </>
+  );
+}
---
+++ app/component/readme.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Readme = () => {
+  return (
+    <div>readme</div>
+  )
+}
+
+export default Readme
